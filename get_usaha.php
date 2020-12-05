aa1d1cd4c894f23c57af2333e215264a4c150443
trioputrap <trioputrap@gmail.com>
2020-12-05 19:36:57 +0800

    update api usaha

--- get_usaha.php
+++ get_usaha.php
@@ -3,7 +3,9 @@
 
     $id_pemilik = $_POST['id_pemilik'];
 
-    $str = "SELECT * FROM perusahaan WHERE id_pemilik='$id_pemilik'";
+    $str = "SELECT perusahaan.*, pengguna.nama as nama_pemilik 
+        FROM perusahaan u INNER JOIN pengguna p ON u.id_pemilik = p.id 
+        WHERE id_pemilik='$id_pemilik'";
     $qry = $conn->query($str);
     if($qry){
         $response = $qry->fetch_all(MYSQLI_ASSOC);
