d21793d23c09a7364899d53f2331a49f48e34ca2
trioputrap <trioputrap@gmail.com>
2020-12-05 19:42:03 +0800

    add get usaha

--- get_usaha.php
+++ get_usaha.php
@@ -3,7 +3,7 @@
 
     $id_pemilik = $_POST['id_pemilik'];
 
-    $str = "SELECT perusahaan.*, pengguna.nama as nama_pemilik 
+    $str = "SELECT u.*, p.nama as nama_pemilik 
         FROM perusahaan u INNER JOIN pengguna p ON u.id_pemilik = p.id 
         WHERE id_pemilik='$id_pemilik'";
     $qry = $conn->query($str);
