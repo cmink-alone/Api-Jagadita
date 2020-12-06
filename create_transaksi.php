5607f594b47719d52b9b686d5bb1cce47f34b8b6
trioputrap <trioputrap@gmail.com>
2020-12-06 16:50:17 +0800

    add new endpoint to create transaksi

---
+++ create_transaksi.php
@@ -0,0 +1,27 @@
+<?php
+    include "connection.php";
+
+    $data = file_get_contents('php://input');
+    $json_data = json_decode($data , true);
+
+    $id_perusahaan = $json_data['id_perusahaan'];
+    $id_pengguna = $json_data['id_pengguna'];
+    $total_beli = $json_data['total_beli'];
+    $tanggal = date('Y-m-d');
+    $status = $json_data['status'];
+
+    $str = "INSERT INTO transaksi VALUES(
+        NULL,
+        '$id_perusahaan',
+        '$id_pengguna',
+        '$total_beli',
+        '$tanggal',
+        '$status'
+    )";
+    $qry = $conn->query($str);
+    if($qry){
+        $response = array('status' => true,'message' => 'Berhasil melakukan transaksi', 'id' => $conn->insert_id);
+    } else {
+        $response = array('status' => false,'message' => 'Gagal melakukan transaksi');
+    }
+    echo json_encode($response);
