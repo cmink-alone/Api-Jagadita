1a5f869f5a8b37bbe49c59267d09a133ae6f6786
trioputrap <trioputrap@gmail.com>
2020-12-06 18:27:54 +0800

    add new endpoint to get transaksi user

--- get_transaksi_user.php
+++ get_transaksi_user.php
@@ -3,7 +3,7 @@
 
     $id_pengguna = $_POST['id_pengguna'];
 
-    $str = "SELECT p.nama as nama_perusahaan, t.total_beli, t.tanggal, status
+    $str = "SELECT p.nama_perusahaan, t.total_beli, t.tanggal, status
         FROM transaksi t INNER JOIN perusahaan p ON t.id_perusahaan = p.id 
         WHERE id_pengguna='$id_pengguna'";
     $qry = $conn->query($str);
