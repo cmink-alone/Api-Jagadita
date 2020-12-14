e2a00bcf6da4e0f787ce45ba5a8decfb5b07dff8
trioputrap <trioputrap@gmail.com>
2020-12-14 17:39:06 +0800

    update get transaksi

--- get_transaksi.php
+++ get_transaksi.php
@@ -5,7 +5,7 @@
 
     $str = "SELECT p.nama as nama_pembeli, t.total_beli, t.tanggal, status
         FROM transaksi t INNER JOIN pengguna p ON t.id_pengguna = p.id 
-        WHERE id_perusahaan='$id_perusahaan'";
+        WHERE id_perusahaan='$id_perusahaan' AND status='berhasil'";
     $qry = $conn->query($str);
     if($qry){
         $response = $qry->fetch_all(MYSQLI_ASSOC);
