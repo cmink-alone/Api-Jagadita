ec7f9d0e92faf147934584e04bb86c4fa071a764
trioputrap <trioputrap@gmail.com>
2020-12-14 17:53:13 +0800

    add donasi endpoint

--- get_donasi.php
+++ get_donasi.php
@@ -3,9 +3,9 @@
 
     $id_perusahaan = $_POST['id_perusahaan'];
 
-    $str = "SELECT p.nama as nama_pembeli, t.total_beli, t.tanggal, status
-        FROM transaksi t INNER JOIN pengguna p ON t.id_pengguna = p.id 
-        WHERE id_perusahaan='$id_perusahaan' AND status='berhasil' ORDER BY t.id DESC";
+    $str = "SELECT p.nama as nama_donatur, d.total_donasi, d.tanggal, status
+        FROM donasi d INNER JOIN pengguna p ON d.id_donatur = p.id 
+        WHERE id_perusahaan='$id_perusahaan' AND status='berhasil' ORDER BY d.id DESC";
     $qry = $conn->query($str);
     if($qry){
         $response = $qry->fetch_all(MYSQLI_ASSOC);
---
+++ get_donasi_user.php
@@ -0,0 +1,15 @@
+<?php
+    include "connection.php";
+
+    $id_pengguna = $_POST['id_pengguna'];
+
+    $str = "SELECT p.nama_perusahaan, d.total_donasi, d.tanggal, status
+        FROM donasi d INNER JOIN perusahaan p ON d.id_perusahaan = p.id 
+        WHERE id_pengguna='$id_pengguna' ORDER BY d.id DESC";
+    $qry = $conn->query($str);
+    if($qry){
+        $response = $qry->fetch_all(MYSQLI_ASSOC);
+    } else {
+        $response = [];
+    }
+    echo json_encode($response);
