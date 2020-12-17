da5e26723ee6b6f803839f148a9ccbf6741e0bc2
trioputrap <trioputrap@gmail.com>
2020-12-17 13:41:02 +0800

    update get donasi

--- get_donasi_user.php
+++ get_donasi_user.php
@@ -5,7 +5,7 @@
 
     $str = "SELECT p.nama_perusahaan, d.total_donasi, d.tanggal, status
         FROM donasi d INNER JOIN perusahaan p ON d.id_perusahaan = p.id 
-        WHERE id_pengguna='$id_pengguna' ORDER BY d.id DESC";
+        WHERE id_donatur='$id_pengguna' ORDER BY d.id DESC";
     $qry = $conn->query($str);
     if($qry){
         $response = $qry->fetch_all(MYSQLI_ASSOC);
